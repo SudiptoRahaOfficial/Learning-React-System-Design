98063ca163dececf36ca49af3dc67dfd7a3d25f3
SudiptoRahaOfficial <sudiptoraha.official@gmail.com>
2026-07-10 15:44:17 +0600

    Handling 403 Unauthorized

---
+++ src/components/ProtectedRoute.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Navigate } from 'react-router'
+import Unauthorized from '../pages/auth/Unauthorized'
+
+function ProtectedRoute({ children, allowedRoles }) {
+	// login from token/locastorage/api
+	const user = { login: true, role: 'user' }
+
+	// 401 -> not logged in
+	if (!user.login) {
+		return <Navigate to='/user-signin' replace />
+	}
+
+	// 403 -> logged in but no premission
+	if (user.role !== 'admin') {
+		return <Unauthorized />
+	}
+	return children
+}
+
+export default ProtectedRoute
--- src/routes/User.routes.jsx
+++ src/routes/User.routes.jsx
@@ -6,11 +6,20 @@ import UserDelete from '../pages/user/UserDelete'
 // import UserList from '../pages/user/UserList'
 const UserList = lazy(() => import('../pages/user/UserList'))
 
+import ProtectedRoute from '../components/ProtectedRoute'
+
 const UserRoutes = [
 	{ path: 'user-add', element: <UserAdd /> },
 	{ path: 'user-update', element: <UserUpdate /> },
 	{ path: 'user-delete', element: <UserDelete /> },
-	{ path: 'user-list', element: <UserList /> },
+	{
+		path: 'user-list',
+		element: (
+			<ProtectedRoute>
+				<UserList />
+			</ProtectedRoute>
+		),
+	},
 ]
 
 export default UserRoutes
