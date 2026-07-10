5faf41aa4fab2664f224cb6ad7c58d8102d26c56
SudiptoRahaOfficial <sudiptoraha.official@gmail.com>
2026-07-10 16:40:58 +0600

    Handling Unexpected Runtime Errors

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -5,14 +5,16 @@ import Unauthorized from '../pages/auth/Unauthorized'
 function ProtectedRoute({ children, allowedRoles }) {
 	// login from token/locastorage/api
 	const user = { login: true, role: 'user' }
+	// uncomment the line below & commentout the line above to see runtime error handling
+	// const user = undefined
 
 	// 401 -> not logged in
-	if (!user.login) {
+	if (!user?.login) {
 		return <Navigate to='/user-signin' replace />
 	}
 
 	// 403 -> logged in but no premission
-	if (user.role !== 'admin') {
+	if (user?.role !== 'admin') {
 		return <Unauthorized />
 	}
 	return children
